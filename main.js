3c4092fef41c91518e2416399a76f1c7073d238b
Yazid ASSOUMANI <y.assoumani@zoho.eu>
2019-02-19 15:44:50 +0000

    Requêtes Ajax avec la méthode GET

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var xhr = new XMLHttpRequest();
+function researchStudent(event, val) {
+    var inputEvent = event.inputType;
+    
+    var letter = val;
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("students").innerHTML = this.responseText;
-    }
-};
+    var xhr = new XMLHttpRequest();
+    
+    if (inputEvent == 'insertText') {
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("students").innerHTML = this.responseText;
+            }
+        };
 
-xhr.open("GET", "students.php");
-xhr.send();
+        var url = 'students.php?prenom=' + letter;
+    
+        xhr.open("GET", url);
+        xhr.send();
+    }    
+}
--- students.php
+++ students.php
@@ -1,23 +1,24 @@
 <?php
 
-if (isset($_GET['prenom'])) {
-    function getStudents() {
-        $prenom = $_GET['prenom'];
-        
-        $connec = new PDO("mysql:dbname=ajax", 'root', '0000');
-        $connec->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $request = $connec->prepare("SELECT nom, prenom 
-                                    FROM students
-                                    WHERE prenom LIKE '%$prenom%';");
-        $request->execute();
-        return $request->fetchAll(PDO::FETCH_ASSOC);
-    }
+function getStudents() {
+    $prenom = $_GET['prenom'];
+    
+    $connec = new PDO("mysql:dbname=ajax", 'root', '0000');
+    $connec->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $request = $connec->prepare("SELECT nom, prenom 
+                                FROM students
+                                WHERE prenom LIKE '$prenom%';");
+    $request->execute();
+    return $request->fetchAll(PDO::FETCH_ASSOC);
+}
 
-    $students = getStudents();
+$students = getStudents();
 
-    foreach ($students as $key => $student) {
-        echo $student['nom'].' '.$student['prenom'].'<br />';
-    }
+foreach ($students as $key => $student) {
+    // $studentLastName = utf8_encode($student['nom']);
+    // $studentFirstName = utf8_encode($student['prenom']);
+
+    echo $student['nom'].' '.$student['prenom'].'<br />';
 }
 
 ?>
